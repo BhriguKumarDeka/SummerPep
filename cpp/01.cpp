5dabe73244aecbd19b18a4d5bebc961a115079b4
BhriguKD <vrigukumar710@gmail.com>
2026-06-22 11:51:26 +0530

    day 1

---
+++ cpp/01.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void kyaTestHoga(int age, int strength){
+//   if( age >= 18 && strength >= 10){
+//     cout <<"Test Conducted" <<"\n";
+//   } else {
+//     cout <<"No Test"<<"\n";
+//   }
+// }
+
+int main(){
+
+  //data types and variables
+  // int -> 4 bytes
+  // long long -> 8 bytes
+  // float -> 4 bytes
+  // double -> 8 bytes
+  // char -> 1 byte
+  // bool -> 1 byte
+
+  // int age;
+  // cout<<age<<endl; // garbage value for uninitialised variable
+
+  // - - - - - - - - - - -
+  
+  // Conditional Statements
+  // kyaTestHoga(20, 11);
+  // kyaTestHoga(17, 11);
+
+  //swtich 
+  // int grade = 'A';
+
+  // switch (grade)
+  // {
+  // case 'A':
+  //   cout<<"A"<<endl;
+  //   break;
+  // case 'B':
+  //   cout<<"B"<<endl;
+  //   break;
+  // case 'C':
+  //   cout<<'C'<<endl;
+  //   break;
+  // default:
+  //   cout<<"default"<<endl;
+  //   break;
+  // }
+
+  // int x=10, y=20;
+  // cout<<(x>y?10:20)<<endl;
+
+  // - - - - - - - - - - - - - 
+
+  //iteration
+
+  // for(int i=0; i< 20; i++){
+  //   cout<<i<<endl;
+  // }
+
+  // int i = 5;
+  // while(i<12){
+  //   cout<<i++;
+  // }
+
+  // int i =0;
+  // do{
+  //   cout<<i++<<endl;
+  // } while(i<5);
+}
